60207aeaf333e8fbe85be5c0529b47df117cb8ec
Rustam <rustamzakirov@mail.ru>
2023-02-07 16:49:04 +0300

    first complete

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Clear();
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Нельзя использовать Math.Pow();
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
 Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите первое число");
